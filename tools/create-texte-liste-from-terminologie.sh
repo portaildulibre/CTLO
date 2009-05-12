fc2f9a347c88421b5612fa183928dad58b1b8af6
Romain PELISSE <belaran@gmail.com>
2009-05-12 15:03:50 +0200

    remove now useless tools script

--- tools/create-texte-liste-from-terminologie.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-TARGET="target/liste.txt"
-grep -e '<anglicisme id="' target/terminologie.xml  | sed -e 's/^<anglicisme id="\(.*\)\(">.*$\)/\1/g' > ${TARGET}
-iconv -f 'UTF-8' -t 'ISO-8859-1'  ${TARGET}  > target/liste.iso8859.1.txt
-unix2dos target/liste.iso8859.1.txt
